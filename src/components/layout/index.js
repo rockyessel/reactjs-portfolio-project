abfac3846bab6865608f28952ac4fa90a48e44c5
Rocky Essel <rockyessel76@gmail.com>
2023-01-26 21:01:07 +0000

    reactjs project portfolio

---
+++ src/components/layout/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sidebar from '../sidebar/index';
+import './index.scss';
+
+const Layout = () => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Layout;
